e4bd317d3eb7da0c536335fe3a78f77b929f5206
June Shaw <june.shaw@me.com>
2017-05-04 23:20:28 -0600

    reorg

---
+++ ioc-and-config-new/src/test/java/com/ioc_and_config/TestWordCounterUnit.java
@@ -0,0 +1,33 @@
+package com.ioc_and_config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestWordCounterUnit {
+
+    @Autowired
+    WordCounter wordCounter;
+
+    @Autowired
+    TestCaseTwo testCase;
+
+    @Test
+    public void testWordCounterUnit() throws Exception{
+        String testString;
+        Map<String, Integer> expectedWordCount = new HashMap<>();
+        testString = testCase.create().getTestString();
+        expectedWordCount = testCase.create().getTestWordCount();
+        Map<String, Integer> actualWordCount = wordCounter.count(testString);
+        assertEquals(actualWordCount, expectedWordCount);
+    }
+}
